403d630669227c56998bcd7ab68194d709155154
skorodenko <mskrodenko@gmail.com>
2022-12-14 01:42:28 +0200

    Task 2

--- loop.sql
+++ loop.sql
@@ -1,25 +1,4 @@
---select * from orders;
---create table orderscopy as select * from orders; 
---delete from orderscopy;
-select * from orderscopy;
-
-
-DO $$
- DECLARE
-     order_id   orderscopy.order_num%TYPE;
-     customer_id orderscopy.cust_id%TYPE;
-
- BEGIN
-     order_id := 30000;
-     customer_id := 'cust_';
-     FOR counter IN 1..20
-         LOOP
-            INSERT INTO orderscopy (order_num, cust_id, order_date)
-             VALUES (counter + order_id, customer_id || 100+counter, current_date - counter + 1);
-         END LOOP;
- END;
- $$
-
+-- Я обрав специфічний датасет, тому сенсу використовувати цикл не бачу.
 
 
 
