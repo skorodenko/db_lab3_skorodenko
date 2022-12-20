3890b33ad5d308ddde053b4954ee895ca71fee61
skorodenko <mskrodenko@gmail.com>
2022-12-20 06:08:55 +0200

    Finished lab_3

--- export_csv.py
+++ export_csv.py
@@ -9,22 +9,16 @@ port = "5432"
 
 OUTPUT_CSV = "skorodenko01_DB_NorthernCitiesClimate.csv"
 
-TABLES = (
-    "country",
-    "hemispheres",
-    "eastwest",
-    "city",
-    "records"
-)
+TABLES = ("country", "hemispheres", "eastwest", "city", "records")
 
 conn = psycopg2.connect(user=username, password=password, dbname=database)
 
 with conn:
     cur = conn.cursor()
     for table_name in TABLES:
-        cur.execute('SELECT * FROM ' + table_name)
+        cur.execute("SELECT * FROM " + table_name)
         fields = [x[0] for x in cur.description]
-        with open(OUTPUT_CSV.format(table_name), 'w') as outfile:
+        with open(OUTPUT_CSV.format(table_name), "w") as outfile:
             writer = csv.writer(outfile)
             writer.writerow(fields)
             for row in cur:
